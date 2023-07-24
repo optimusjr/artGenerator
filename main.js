caec4bbb2d09b0abf0466c01522b17d66d9c142e
Silas Henrique <silash35@gmail.com>
2023-07-24 20:27:19 -0300

    fix not loading function

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import { h, Component, render } from "https://esm.sh/preact";
 import htm from "https://esm.sh/htm";
 
-console.log("AAAAA");
-
 // Initialize htm with Preact
 const html = htm.bind(h);
 
@@ -10,8 +8,16 @@ function App(props) {
   return html`<h1>Hello ${props.name}!</h1>`;
 }
 
-const root = document.getElementById("preact");
+const loadRoot = () => {
+  console.log("loading root");
 
-console.log(root);
+  const root = document.getElementById("preact");
+  if (root === null) {
+    console.log("No root element, trying again in one second");
+    window.setTimeout(loadRoot, 1000);
+  } else {
+    render(html`<${App} name="World" />`, root);
+  }
+};
 
-render(html`<${App} name="World" />`, root);
+window.onload(loadRoot);
