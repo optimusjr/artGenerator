2d35b5b9749255a56dd919979f122f121567fd2c
Silas Henrique <silash35@gmail.com>
2023-08-07 21:53:27 -0300

    update svg

--- app.tsx
+++ app.tsx
@@ -4,7 +4,8 @@ import VirtualSignature from "./components/arts/VirtualSignature";
 import DownloadButton from "./components/DownloadButton";
 
 const App = () => {
-  const [name, setName] = useState("Seu Nome");
+  const [name, setName] = useState("Sam Sumitpo");
+  const [role, setRole] = useState("Seu Cargo");
   const virtualSignatureRef = useRef(null);
 
   const getVirtualSignatureElement = () => {
@@ -14,17 +15,34 @@ const App = () => {
   return (
     <div>
       <h1>Art Generator</h1>
+      <p>
+        Gerador de Imagens para a OPTIMUS Jr. Veja mais informações no
+        <a href="https://github.com/optimusjr/artGenerator">GitHub do Projeto</a>
+      </p>
 
-      <label>
-        <p>Seu Nome</p>
-        <input onChange={(e) => setName((e.target as HTMLInputElement).value)} value={name} />
-      </label>
+      <h2 className="tabset">Imagens</h2>
 
-      <DownloadButton getElement={getVirtualSignatureElement} />
+      <article>
+        <h3>Assinatura Virtual</h3>
 
-      <div ref={virtualSignatureRef} style={{ lineHeight: 0 }}>
-        <VirtualSignature fontSize={64} name={name} />
-      </div>
+        <form>
+          <label style={{ display: "flex", gap: 8 }}>
+            <p>Seu Nome</p>
+            <input onChange={(e) => setName((e.target as HTMLInputElement).value)} value={name} />
+          </label>
+
+          <label style={{ display: "flex", gap: 8 }}>
+            <p>Seu Cargo</p>
+            <input onChange={(e) => setRole((e.target as HTMLInputElement).value)} value={role} />
+          </label>
+
+          <DownloadButton getElement={getVirtualSignatureElement} />
+        </form>
+
+        <div ref={virtualSignatureRef} style={{ lineHeight: 0 }}>
+          <VirtualSignature name={name} role={role} />
+        </div>
+      </article>
     </div>
   );
 };
