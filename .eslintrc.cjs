d9fdda33b6a923058d6cc61ec972dfb0eaee245c
Silas Henrique <silash35@gmail.com>
2023-08-07 22:08:28 -0300

    fix eslint blocking

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const eslintConfig = require("eslint-config-silash35");
 
+eslintConfig.ignorePatterns.push("*.js");
+
 module.exports = eslintConfig;
