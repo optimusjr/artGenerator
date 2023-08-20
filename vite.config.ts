588248479c33e923b15345ebf3cf74a417a57384
Silas Henrique <silash35@gmail.com>
2023-08-19 21:32:49 -0300

    add src folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+  },
   plugins: [preact()],
 });
