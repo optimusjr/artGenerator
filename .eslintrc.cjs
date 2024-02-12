1c2ab091e5ce056e1385eb3bb3ce117cf796e221
Silas Henrique <silash35@gmail.com>
2024-02-11 21:48:04 -0300

    update pnpm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const eslintConfig = require("eslint-config-silash35");
 
-eslintConfig.ignorePatterns.push("*.js");
-
 module.exports = eslintConfig;
